959ad24777c25230fe3008f08cdd1115a13a058f
saruman9 <rum.274.4@gmail.com>
2020-10-19 23:52:51 +0300

    Temporary remove errors (Result return)

--- src/boolean_expression.rs
+++ src/boolean_expression.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::error::Error;
 
 use pest::iterators::Pair;
 use pest::prec_climber::{Assoc, Operator, PrecClimber};
@@ -12,10 +11,7 @@ pub struct BooleanExpressionParser;
 
 type Definitions = HashMap<String, String>;
 
-pub fn parse_boolean_expression(
-    input: &str,
-    definitions: &Definitions,
-) -> Result<bool, Box<dyn Error>> {
+pub fn parse_boolean_expression(input: &str, definitions: &Definitions) -> Result<bool, String> {
     let mut expr =
         BooleanExpressionParser::parse(Rule::boolean_expression, input).or(Err("Parser"))?;
     Ok(parse_expr(expr.next().unwrap(), definitions))
@@ -30,7 +26,7 @@ pub fn parse_expr(pair: Pair<Rule>, definitions: &Definitions) -> bool {
     consume(pair, &climber, definitions)
 }
 
-pub fn consume(pair: Pair<Rule>, climber: &PrecClimber<Rule>, definitions: &Definitions) -> bool {
+fn consume(pair: Pair<Rule>, climber: &PrecClimber<Rule>, definitions: &Definitions) -> bool {
     let primary = |pair| consume(pair, climber, definitions);
     let infix = |l: bool, op: Pair<Rule>, r: bool| match op.as_rule() {
         Rule::OR_OP => l || r,
@@ -41,67 +37,55 @@ pub fn consume(pair: Pair<Rule>, climber: &PrecClimber<Rule>, definitions: &Defi
 
     match pair.as_rule() {
         Rule::expr => climber.climb(pair.into_inner(), primary, infix),
-        Rule::boolean_clause => parse_boolean_clause(pair, definitions).unwrap(),
+        Rule::boolean_clause => parse_boolean_clause(pair, definitions),
         _ => unreachable!(pair),
     }
 }
 
-pub fn parse_boolean_clause(
-    pair: Pair<Rule>,
-    definitions: &Definitions,
-) -> Result<bool, Box<dyn Error>> {
+pub fn parse_boolean_clause(pair: Pair<Rule>, definitions: &Definitions) -> bool {
     let pair = pair.into_inner().next().unwrap();
     match pair.as_rule() {
         Rule::expr_not => parse_expr_not(pair, definitions),
-        Rule::expr_paren => Ok(parse_expr_paren(pair, definitions)),
+        Rule::expr_paren => parse_expr_paren(pair, definitions),
         Rule::expr_eq => parse_expr_eq(pair, definitions),
         Rule::expr_defined => parse_expr_defined(pair, definitions),
         _ => unreachable!(pair),
     }
 }
 
-pub fn parse_expr_not(pair: Pair<Rule>, definitions: &Definitions) -> Result<bool, Box<dyn Error>> {
-    Ok(!parse_expr_paren(
-        pair.into_inner().next().unwrap(),
-        definitions,
-    ))
+pub fn parse_expr_not(pair: Pair<Rule>, definitions: &Definitions) -> bool {
+    !parse_expr_paren(pair.into_inner().next().unwrap(), definitions)
 }
 
 pub fn parse_expr_paren(pair: Pair<Rule>, definitions: &Definitions) -> bool {
     parse_expr(pair.into_inner().next().unwrap(), definitions)
 }
 
-pub fn parse_expr_eq(pair: Pair<Rule>, definitions: &Definitions) -> Result<bool, Box<dyn Error>> {
+pub fn parse_expr_eq(pair: Pair<Rule>, definitions: &Definitions) -> bool {
     let mut inner = pair.into_inner();
-    let l = parse_expr_term(inner.next().unwrap(), definitions)?;
+    let l = parse_expr_term(inner.next().unwrap(), definitions);
     let comp_op = inner.next().unwrap();
-    let r = parse_expr_term(inner.next().unwrap(), definitions)?;
+    let r = parse_expr_term(inner.next().unwrap(), definitions);
     match comp_op.as_str() {
-        "!=" => Ok(l != r),
-        "==" => Ok(l == r),
+        "!=" => l != r,
+        "==" => l == r,
         _ => unreachable!(comp_op),
     }
 }
 
-pub fn parse_expr_defined(
-    pair: Pair<Rule>,
-    definitions: &Definitions,
-) -> Result<bool, Box<dyn Error>> {
+pub fn parse_expr_defined(pair: Pair<Rule>, definitions: &Definitions) -> bool {
     let identifier_pair = pair.into_inner();
-    Ok(definitions.contains_key(identifier_pair.as_str()))
+    definitions.contains_key(identifier_pair.as_str())
 }
 
-pub fn parse_expr_term(
-    pair: Pair<Rule>,
-    definitions: &Definitions,
-) -> Result<String, Box<dyn Error>> {
+pub fn parse_expr_term(pair: Pair<Rule>, definitions: &Definitions) -> String {
     let pair = pair.into_inner().next().unwrap();
     match pair.as_rule() {
         Rule::IDENTIFIER => {
             let key = pair.as_str();
-            Ok(definitions.get(key).cloned().unwrap_or_default())
+            definitions.get(key).cloned().unwrap()
         }
-        Rule::STRING => Ok(pair.as_str().to_string()),
+        Rule::STRING => pair.as_str().to_string(),
         _ => unreachable!(pair),
     }
 }
