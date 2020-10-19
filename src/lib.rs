49df5f46dea8b68bfe0fdf6b04cc68dc1a760e2d
saruman9 <rum.274.4@gmail.com>
2020-10-19 23:54:06 +0300

    Add asserts to tests, add third panic test

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,16 @@ pub struct SleighParser;
 
 #[cfg(test)]
 mod tests {
-    use crate::boolean_expression::parse_boolean_expression;
     use std::collections::HashMap;
+
+    use crate::boolean_expression::parse_boolean_expression;
+
     #[test]
     fn first() {
         let input = r#"(SH_VERSION == "2") || (SH_VERSION == "2A")"#;
         let mut definitions = HashMap::new();
         definitions.insert("SH_VERSION".to_string(), "2".to_string());
-        dbg!(parse_boolean_expression(input, &definitions).unwrap());
+        assert_eq!(parse_boolean_expression(input, &definitions).unwrap(), true);
     }
 
     #[test]
@@ -23,6 +25,20 @@ mod tests {
         let input = "(defined(dsPIC24F) || defined(dsPIC33E)) && defined(dsPIC33C)";
         let mut definitions = HashMap::new();
         definitions.insert("dsPIC24F".to_string(), Default::default());
-        dbg!(parse_boolean_expression(input, &definitions).unwrap());
+        assert_eq!(
+            parse_boolean_expression(input, &definitions).unwrap(),
+            false
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn third() {
+        let input = r#"(defined(test) || defined(ti)) && PROCESSOR == "PIC_16""#;
+        let definitions = HashMap::new();
+        assert_eq!(
+            parse_boolean_expression(input, &definitions).unwrap(),
+            false
+        );
     }
 }
