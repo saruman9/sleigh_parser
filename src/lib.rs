4e7efe153d82cdc1f6cdace82a2d6181c1a2aef0
saruman9 <rum.274.4@gmail.com>
2020-10-20 23:41:22 +0300

    Rename tests, add "definitions" test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use pest_derive::Parser;
 
 pub mod boolean_expression;
+pub mod errors;
 
 #[derive(Parser)]
 #[grammar = "sleigh.pest"]
@@ -11,17 +12,18 @@ mod tests {
     use std::collections::HashMap;
 
     use crate::boolean_expression::parse_boolean_expression;
+    use crate::errors::Error;
 
     #[test]
-    fn first() {
-        let input = r#"(SH_VERSION == "2") || (SH_VERSION == "2A")"#;
+    fn eq_or() {
+        let input = r#"SH_VERSION == "2" || SH_VERSION == "2A""#;
         let mut definitions = HashMap::new();
         definitions.insert("SH_VERSION".to_string(), "2".to_string());
         assert_eq!(parse_boolean_expression(input, &definitions).unwrap(), true);
     }
 
     #[test]
-    fn second() {
+    fn paren() {
         let input = "(defined(dsPIC24F) || defined(dsPIC33E)) && defined(dsPIC33C)";
         let mut definitions = HashMap::new();
         definitions.insert("dsPIC24F".to_string(), Default::default());
@@ -32,13 +34,21 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
-    fn third() {
+    fn not_defined() {
         let input = r#"(defined(test) || defined(ti)) && PROCESSOR == "PIC_16""#;
-        let definitions = HashMap::new();
         assert_eq!(
-            parse_boolean_expression(input, &definitions).unwrap(),
-            false
+            parse_boolean_expression(input, &Default::default())
+                .err()
+                .unwrap(),
+            Error::NotDefined("PROCESSOR".to_string())
         );
+    }
+
+    #[test]
+    fn definitions() {
+        let input = r#"DATA_ENDIAN == "little""#;
+        let mut definitions = HashMap::new();
+        definitions.insert("DATA_ENDIAN".to_string(), "little".to_string());
+        assert_eq!(parse_boolean_expression(input, &definitions).unwrap(), true);
     }
 }
