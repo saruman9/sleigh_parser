ba152579cb0d3dd1e43bf5a1b423879a4d3ec05a
saruman9 <rum.274.4@gmail.com>
2020-11-11 10:14:35 +0300

    Use `sleigh_preprocessor` as dependence

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::{env::args, fs::read_to_string};
+use std::{collections::HashMap, env::args};
 
 use sleigh_parser::parser::lexer::Tokenizer;
+use sleigh_preprocessor::SleighPreprocessor;
 
 fn main() {
     env_logger::builder()
         .target(env_logger::Target::Stdout)
         .init();
-    let input = read_to_string(args().nth(1).unwrap()).unwrap();
-    let tokens = Tokenizer::new(&input);
+
+    let mut writer = String::new();
+    let mut definitions = HashMap::new();
+    let file_path = args().nth(1).unwrap();
+    let mut sleigh_preprocessor = SleighPreprocessor::new(definitions, &file_path);
+
+    definitions = match sleigh_preprocessor.process(&mut writer) {
+        Ok(def) => def,
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    };
+    println!("{:?}", definitions);
+
+    let tokens = Tokenizer::new(&writer);
     for token in tokens {
         println!("{:?}", token);
     }
