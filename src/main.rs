c9829e892b0f1662b7181e4892a53fa32aff8703
saruman9 <rum.274.4@gmail.com>
2020-11-24 23:47:47 +0300

    Create test binary

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+use pest::Parser;
+use sleigh_preprocessor::SleighPreprocessor;
+
+use sleigh_parser::{Rule, SleighParser};
+
+fn main() {
+    let mut writer = String::new();
+    let mut preprocessor =
+        SleighPreprocessor::new(HashMap::new(), std::env::args().nth(1).unwrap(), true);
+    let (definitions, locations) = match preprocessor.process(&mut writer) {
+        Ok((def, loc)) => (def, loc),
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("{:#?}", &definitions);
+    println!("{:#?}", &locations);
+
+    match SleighParser::parse(Rule::sleigh, &writer) {
+        Ok(parser) => {
+            println!("{:#?}", parser.tokens());
+        }
+        Err(e) => eprintln!("{}", e),
+    }
+}
